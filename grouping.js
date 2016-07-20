bed6d84c6de4d40b2c2558b2a1aa1bee97ee4e2b
Artur Barcicki <barcicki@gmail.com>
2016-07-20 20:50:55 +0200

    Comments

--- grouping.js
+++ grouping.js
@@ -1,3 +1,10 @@
+/**
+ * Checks if provided items is an array of strings or an object with all
+ * properties that are arrays of strings.
+ *
+ * @param {*} items
+ * @returns {boolean}
+ */
 function canGroup(items) {
     if (Array.isArray(items)) {
         return isArrayOfStrings(items);
@@ -13,21 +20,41 @@ function canGroup(items) {
     return false;
 }
 
+/**
+ * Checks if provided items is an array of strings.
+ *
+ * @param {*} items
+ * @returns {boolean}
+ */
 function isArrayOfStrings(items) {
     return Array.isArray(items) && items.every(item => typeof item === 'string');
 }
 
-function createToWeightedItemConverter(additionalParams) {
+/**
+ * Creates mapper function able to extract weight from string.
+ *
+ * @param {object} params - additional parameters to be included in the output
+ * @returns {function(string)}
+ */
+function createToWeightedItemConverter(params = {}) {
     return (string) => {
         const [item, weight] = string.split(',');
 
         return Object.assign({
             item,
             weight: weight ? parseInt(weight, 10) : 0
-        }, additionalParams);
+        }, params);
     }
 }
 
+/**
+ * Comparator for WeightedItem.
+ * Orders items by weight or randomly if weight is the same.
+ *
+ * @param {WeightedItem} itemA
+ * @param {WeightedItem} itemB
+ * @returns {number}
+ */
 function sortByWeight(itemA, itemB) {
     if (itemA.weight === itemB.weight) {
         return 0.5 - Math.random();
@@ -36,6 +63,14 @@ function sortByWeight(itemA, itemB) {
     return itemB.weight - itemA.weight;
 }
 
+/**
+ * Flattens the list if composed of baskets.
+ * Extracts weight from strings and converts items to WeightedItem.
+ * Shuffles the list based on weight and basket ownership.
+ *
+ * @param {object|Array.<string>} data - data to divide
+ * @returns {Array.<WeightedItem>}
+ */
 function convertAndShuffleItems(data) {
     if (Array.isArray(data)) {
         return data
@@ -55,12 +90,20 @@ function convertAndShuffleItems(data) {
         }, []);
 }
 
+/**
+ * Divides items from data into certain number of groups.
+ *
+ * @param {object|Array.<string>} data - data to divide
+ * @param {number} [maxGroupSize=2] - max numbers of items in group
+ * @param {boolean} [isStrict=false] - flag informing whether the script should
+ * check if the data can be evenly divided
+ * @returns {Array.<Array.<string>>}
+ */
 function group(data, { maxGroupSize = 2, isStrict = false } = {}) {
     const items = convertAndShuffleItems(data);
 
     if (maxGroupSize === 1) {
-        return items
-            .map(weightedItem => [weightedItem.item]);
+        return items.map(weightedItem => [weightedItem.item]);
     }
 
     if (isStrict && items.length % maxGroupSize !== 0) {
@@ -88,3 +131,10 @@ module.exports = {
     canGroup,
     group
 };
+
+/**
+ * @typedef {object} WeightedItem
+ * @property {string} item
+ * @property {number} weight
+ * @property {string} [basket]
+ */
